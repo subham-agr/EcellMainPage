67a8a9fe06a2545e028df554121b09e9091390ba
subham-agr <agrsubham423@gmail.com>
2022-12-19 21:21:06 +0530

    Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './Components/navbar/navbar.component'
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
+
+const material = [
+  MatToolbarModule,
+  MatGridListModule,
+  MatIconModule,
+  MatCardModule
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    material
+  ],
+  exports: [
+    material
   ],
   providers: [],
   bootstrap: [AppComponent]
